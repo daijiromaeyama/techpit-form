310bed8350e1ff156c0cbabc65022b87279387fa
daijiro.maeyama <daijiro.maeyama13@gmail.com>
2020-05-12 16:46:01 +0900

    add address form

--- front/src/store/profile/actions.ts
+++ front/src/store/profile/actions.ts
@@ -5,7 +5,7 @@ import { Address } from "../../domain/entity/address";
 const actionCreator = actionCreatorFactory();
 
 const profileActions = {
-  setProfile: actionCreator<Partial<Profile>>("SET_PROFILE")
+  setProfile: actionCreator<Partial<Profile>>("SET_PROFILE"),
   setAddress: actionCreator<Partial<Address>>("SET_ADDRESS")
 };
 
